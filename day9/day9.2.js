be0c091336f5b1b33fe89f03acff5f9e5609a6b9
Radu Lupaescu <radu.lupaescu@gmail.com>
2023-12-11 15:57:17 +0200

    day 9

--- day9/day9.2.js
+++ day9/day9.2.js
@@ -2,6 +2,32 @@ const fs = require('fs');
 const path = require("path");
 
 fs.readFile(path.join(__dirname, 'day9.input'), 'utf8', (err, data) => {
+    let sum = 0;
     data.split("\n").forEach((line, index) => {
+        const values = line.split(' ').map((value) => parseInt(value));
+
+        sum += values[0] - reduce(values);
     });
+
+    console.log(sum);
 });
+
+const reduce = (values) => {
+    const reduced = [];
+    let allZeroes = true;
+    for (let i = 0; i < values.length - 1; i++) {
+        const diff = values[i + 1] - values[i];
+        if (diff !== 0) {
+            allZeroes = false;
+        }
+
+        reduced.push(diff);
+    }
+
+    let diff = 0;
+    if (!allZeroes) {
+        diff = reduce(reduced);
+    }
+
+    return reduced[0] - diff;
+};
