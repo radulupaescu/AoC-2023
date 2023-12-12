47cdff74b097a81d3cbe299f93e6243b6103ce49
Radu Lupaescu <radu.lupaescu@gmail.com>
2023-12-12 23:40:35 +0200

    minor update to template

--- day0/day0.1.js
+++ day0/day0.1.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require("path");
 
-fs.readFile(path.join(__dirname, 'day0.input'), 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, 'day.input'), 'utf8', (err, data) => {
     data.split("\n").forEach((line, index) => {
     });
 });
