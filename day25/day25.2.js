e5ad15a924fc9f3168ac656e2bb3bbe4fff66481
Radu Lupaescu <radu.lupaescu@gmail.com>
2023-12-27 12:29:13 +0200

    removed 25.2

--- day25/day25.2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-fs.readFile(path.join(__dirname, 'day.input'), 'utf8', (err, data) => {
-    data.split("\n").forEach((line, index) => {
-    });
-});
